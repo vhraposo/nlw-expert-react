68167c64e14aa3adfca3f439468e4fb476266bc1
vhraposo <thevhup@gmail.com>
2024-02-14 17:46:08 -0300

    refactor: NewNoteCard component to use props

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,7 +3,12 @@ import { X } from 'lucide-react'
 import { ChangeEvent, FormEvent, useState } from 'react'
 import { toast } from 'sonner'
 
-export function NewNoteCard(){
+interface NewNoteCardProps {
+    onNoteCreated: (content: string) => void
+
+}
+
+export function NewNoteCard({ onNoteCreated } : NewNoteCardProps){
     const [shouldShowOnboarding, setshouldShowOnboarding ] = useState(true)
     const [content, setContent] = useState('')
 
@@ -20,8 +25,10 @@ export function NewNoteCard(){
 
     function handleSaveNote(event: FormEvent){
         event.preventDefault()
-        console.log(content)
+        onNoteCreated(content)
 
+        setContent('')
+        setshouldShowOnboarding(true)
         toast.success('Nota criada com sucesso!')
     }
 
@@ -52,6 +59,7 @@ export function NewNoteCard(){
                                 autoFocus 
                                 className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none' 
                                 onChange={handleContentChange}
+                                value={content}
                                 />
                             )
                             }              
