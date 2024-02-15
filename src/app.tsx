f215b19ffeec506ad0d47450bfb20804b1bc37cc
vhraposo <thevhup@gmail.com>
2024-02-14 21:58:10 -0300

    refactor: note deletion logic and update grid layout

--- src/app.tsx
+++ src/app.tsx
@@ -38,12 +38,20 @@ export function App() {
     setSearch(query)
   }
 
+  function onNoteDeleted(id: string){
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+    setNotes(notesArray)
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
   const filteredNotes = search !== '' 
   ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) 
   : notes
 
   return  (
-    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+    <div className='mx-auto max-w-6xl my-12 space-y-6 px-5'>
       <img src={ logo } alt='Nlw Expert' />
 
       <form className='w-full'>
@@ -57,11 +65,11 @@ export function App() {
       </form>
       <div className='h-px bg-slate-700' />
 
-      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]'>
           <NewNoteCard onNoteCreated={onNoteCreated} />   
          
          {filteredNotes.map(note => {
-            return <NoteCard key={note.id} note={note} />
+            return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />
          })}
 
       </div>
